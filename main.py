5bc900395e349f70b3e3e01b153ddf803050a689
hafiz <hafizalimi77@gmail.com>
2022-02-09 16:10:40 +0000

    init

---
+++ main.py
@@ -0,0 +1,5 @@
+print('Welcome to the Band Name Generator .')
+quest1 = input("What's name of the city you grew up in ?\n")
+quest2 = input("Whats your pet name ?\n")
+answer = quest1 + ' ' + quest2
+print("Your band name could be",answer)
